a6216ac0e5edfab7bac830e0113726d3617c391f
rocketmike12 <mg.korop@gmail.com>
2025-07-16 17:49:39 +0300

    trying to deploy vite && react #4

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "rocketmike12.github.io/goiteens_fe_5_hw01"
+	base: "/goiteens_fe_5_hw01"
 });
