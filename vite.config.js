8dca2b7de9cbe82939049a71040b86ca677b4b4f
rocketmike12 <mg.korop@gmail.com>
2025-07-16 17:19:55 +0300

    trying to deploy vite && react

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import glob from "fast-glob";
+import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	base: "https://rocketmike12.github.io/goiteens_fe_5_hw01/",
+	plugins: [react()],
+	build: {
+		minify: false,
+		rollupOptions: {
+			input: {
+				...Object.fromEntries(
+				glob
+					.sync(["./*.html", "./pages/**/*.html"])
+					.map((file) => [path.relative(__dirname, file.slice(0, file.length - path.extname(file).length)), fileURLToPath(new URL(file, import.meta.url))])
+				),
+				...{author: path.resolve("author", "index.html")}
+			},
+			output: {
+				assetFileNames: "assets/[name].[ext]",
+			},
+		},
+	},
+});
